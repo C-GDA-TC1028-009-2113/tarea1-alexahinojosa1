d10728c9a4377b54459b789805cd80a079d38cd7
alexahinojosa1 <a01641508@itesm.mx>
2021-08-22 16:40:52 +0000

    peso

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    peso_i= float(input("Dame el peso inicial: "))
+    peso_f= float(input("Dame el peso final: "))
+    meses= int(input("Dame la cantidad de meses: "))
+     
+    meta= peso_i - peso_f
+    bajarpormes= meta/meses
+    float(bajarpormes)
+    print("Lo que debes bajar por mes es:",bajarpormes)
+
+
 
 
 
