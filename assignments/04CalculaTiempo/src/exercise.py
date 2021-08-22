a9144afa2ef0edb2b142858e0e420de01db3fe87
alexahinojosa1 <a01641508@itesm.mx>
2021-08-22 07:56:29 +0000

    calculatiempo

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+edad= int(input("Dame tu edad: "))
+año =int(input("Dame el año actual: "))
+
+faltante =100-edad
+año_final= faltante+año
+
+print ("Cumplirás 100 años en el año:",año_final)
+
 
 
 
