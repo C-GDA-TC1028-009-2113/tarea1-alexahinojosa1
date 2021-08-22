991314884fdb507d086a9ebe784a4a9feb8d1982
alexahinojosa1 <a01641508@itesm.mx>
2021-08-22 16:58:00 +0000

    videojuego

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
+    vnuevos= int(input("Dame la cantidad de juegos nuevos: "))
+    vusados= int(input("Dame la cantidad de juegos usados: " ))
+    
+    total = (vnuevos*1000)+(vusados*350)
+    print ("El total de la compra es:", total)
 
 if __name__ == '__main__':
     main()
